6cb1bdc9a49bc714cc235279d6d02d7ac4638edd
EgorN <novenkov0106@yandex.ru>
2022-04-05 22:29:51 +0500

    The firs commit

---
+++ src/packege_1/Task_5.java
@@ -0,0 +1,19 @@
+package packege_1;
+
+public class Task_5 {
+
+    public static void main(String[] args) {
+
+
+
+        for (int i = 0; i < 0; i++) {
+            System.out.println(i);
+        }
+
+
+    }
+
+
+
+
+}
